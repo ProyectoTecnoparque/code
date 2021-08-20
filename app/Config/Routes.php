4bb8d06769e77f7995b77b08d1ad02084069b3ad
ProyectoTecnoparque <proyectostecnoparquepereira@gmail.com>
2021-08-20 15:53:39 -0500

    changes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -40,7 +40,11 @@ $routes->get('/cargarVistaInicio', 'Index::cargarVistaInicio');
 $routes->get('/ValidarDatosIngreso', 'Index::ValidarDatosIngreso');
 $routes->get('/Puntos', 'Puntos::historial_punto');
 $routes->get('/Historial', 'Historial::historial_expe');
+
 $routes->get('/BuscarUsuarios','BuscarUsuarios:: listar_usuarios');
+
+$routes->get('/BuscarId','BuscarUsuarios:: Buscar_id');
+
 
 // $routes->group('ModuloUsuario', ['namespace' => 'App\Controllers\ModuloUsuario'], function ($routes) {
 // 	$routes->get('BuscarUsuarios','BuscarUsuarios:: index');
